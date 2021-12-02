61b31b7c1810fc17c8d890cba1408e03958295fe
ocelot8421 <8ocelot@gmail.com>
2021-12-02 21:20:42 +0100

    list.basiclist List01 is done

--- src/main/java/hu/progmasters/list/basiclist/List01.java
+++ src/main/java/hu/progmasters/list/basiclist/List01.java
@@ -1,17 +1,27 @@
 package hu.progmasters.list.basiclist;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 public class List01 {
+
     public static void main(String... args) {
         List<Object> arrayList = new ArrayList<>();
         // Add multiple elements to the list and print the number of the elements
         // Try multiple approach:
         // - 1 by 1
+        arrayList.add(6);
+        arrayList.add("alma");
+        arrayList.add(" van");
+        System.out.println(arrayList);
+        System.out.println(arrayList.size());
         // - via the constructor of the ArrayList
+        List<Object> arrayList2 = new ArrayList<>(List.of(" mindenhol", " a", " földön"));
+        System.out.println(arrayList2);
         // - via addAll() method
-        System.out.println();
+        arrayList.addAll(arrayList2);
+        System.out.println(arrayList);
     }
 
 }
