7db22f59dcaf90971ccf6f7d60c69c4b6864e619
ocelot8421 <8ocelot@gmail.com>
2021-12-03 14:52:22 +0100

    list.basiclist List02 is done

--- src/main/java/hu/progmasters/list/basiclist/List02.java
+++ src/main/java/hu/progmasters/list/basiclist/List02.java
@@ -1,14 +1,15 @@
 package hu.progmasters.list.basiclist;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class List02 {
     public static void main(String... args) {
         String[] numbers = new String[]{"one", "two", "three", "four", "five"};
         // Initialize the arrayList using the numbers array
-        List arrayList = new ArrayList(); // ??
-
+        List<String> arrayList = new ArrayList<>(); // ??
+        arrayList.addAll(Arrays.asList(numbers));
         System.out.println(arrayList);
     }
 }
