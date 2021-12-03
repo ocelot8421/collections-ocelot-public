f4cda5f13324abf04c34cbe560952cc852d14b6d
ocelot8421 <8ocelot@gmail.com>
2021-12-03 23:58:51 +0100

    list.basiclist List09 is done

--- src/main/java/hu/progmasters/list/basiclist/List09.java
+++ src/main/java/hu/progmasters/list/basiclist/List09.java
@@ -1,14 +1,16 @@
 package hu.progmasters.list.basiclist;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class List09 {
 
     public static void main(String... args) {
         List<String> al = new ArrayList<>(Arrays.asList("kuty", "macsk", "kacs", "rók"));
         // Add "a" to every string in 'al'. Use an Iterator!
+        ListIterator<String> allIterator = al.listIterator();
+        while (allIterator.hasNext()) {
+           allIterator.set(allIterator.next() + "a");
+        }
         // Hint: the Iterator interface doesn't have the capability to achieve this,
         // so you can use the ListIterator's 'set' method.
 
